83082d70c675c3ccf7d25dfaf88890573efa0580
NawarYossef <nawarpianist@gmail.com>
2016-06-20 07:16:16 -0700

    Update exercise 6.rb

--- class 95/basics/exercise 6.rb
+++ class 95/basics/exercise 6.rb
@@ -1,6 +1,6 @@
-  float1 = 3.33
-  float2 = 4.67
-  float3 = 5.5
-  puts float1**2
-  puts float2**2
-  puts float3**2
+float1 = 3.33
+float2 = 4.67
+float3 = 5.5
+puts float1**2
+puts float2**2
+puts float3**2
