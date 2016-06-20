060a5b3b25b58079496384db453cd695246bfa8d
NawarYossef <nawarpianist@gmail.com>
2016-06-20 07:12:02 -0700

    Update exercise 2.rb

--- class 95/arrays/exercise 2.rb
+++ class 95/arrays/exercise 2.rb
@@ -1,6 +1,6 @@
-  #I looked up the solution and video walkthrow for this one
-  1. returns 1
-    arr =  [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
+#I looked up the solution and video walkthrow for this one
+1. returns 1
+arr =  [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
 
-  2. returns [1, 2, 3]
-    arr = [["b"], ["a", [1, 2, 3]]]
+2. returns [1, 2, 3]
+arr = [["b"], ["a", [1, 2, 3]]]
