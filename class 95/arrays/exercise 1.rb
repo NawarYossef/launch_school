9a2f3f848a9b52c24778e5cd35613d265fb5f267
NawarYossef <nawarpianist@gmail.com>
2016-06-20 07:11:26 -0700

    Update exercise 1.rb

--- class 95/arrays/exercise 1.rb
+++ class 95/arrays/exercise 1.rb
@@ -1,9 +1,9 @@
-  arr = [1, 3, 5, 7, 9, 11]
+arr = [1, 3, 5, 7, 9, 11]
 
 
-  if  arr.include? (3)
-  puts "Array includes the number 3"
-  else
-  puts "Array does not include the number 3"
-  end
+if  arr.include? (3)
+puts "Array includes the number 3"
+else
+puts "Array does not include the number 3"
+end
 
