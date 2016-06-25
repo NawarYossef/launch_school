57c6eafa40192c4a68d14166b47863cc3e1756aa
Nawar Yossef <nawarpianist@gmail.com>
2016-06-24 17:59:29 -0700

    file added

---
+++ class 95/hashes/exercise 5.rb
@@ -0,0 +1,7 @@
+h1 = {name: "Max Martin", job: "Music Producer", net: "250 Million"}
+    puts " Does that Hash include tha name of Max Martin?"
+ if h1.has_value?("Max Martin")
+     puts " yes"
+ else
+     puts "No"
+ end
