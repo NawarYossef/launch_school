5b3ee3605098d96b9eb18f2d1e4732f4cbb6e251
Nawar Yossef <nawarpianist@gmail.com>
2016-06-24 17:57:01 -0700

    file added

---
+++ class 95/flow control/exercise 6.rb
@@ -0,0 +1,3 @@
+ The error is because an 'end' was expected to end the equal_to_four method. 
+ The solution is to add an 'end' keyword in line 7 after the 'end' in line 6.
+ This will close off both the conditional statement and the method. 
