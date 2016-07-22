375d92d45a7037f57536aee9d4642c5eb32299ab
Nawar Yossef <nawarpianist@gmail.com>
2016-07-21 19:02:41 -0700

    game update-bonus features

--- class 100/lesson_3/rock paper scissors _game.rb
+++ class 100/lesson_3/rock paper scissors _game.rb
@@ -1,67 +1,58 @@
-WINNING_COMBINATION = { "rock" => %w(lizard scissors),
-                        "paper" => %W(rock spock),
-                        "scissors" => %w(paper lizard),
-                        "lizard" => %w(spock paper),
-                        "spock" => %w(scissors rock)
-
+WIN_CHOICES = {
+  "rock" => %W(lizard scissors),
+  "paper" => %w(rock, spock),
+  "scissors" => %W(lizard paper),
+  "lizard" => %W(paper spock),
+  "spock" => %W(scissors rock)
 }.freeze
 
-Computer_choices =
-  %w(rock paper scissors lizard spock).sample
-
-Convert_choices = {
-  'r' => 'rock',
-  'p' => 'paper',
-  's' => 'scissors',
-  'l' => 'lizard',
-  'sp' => 'spock'
+PLAYER_CONVERT_CHOICES = {
+  "r" => 'rock',
+  "p" => 'paper',
+  "s" => "scissors",
+  "l" => "lizard",
+  "sp" => "spock"
 }.freeze
 
-Result_display = {
-  %w(rock lizard) => "rock beats lizard",
-  %w(rock scissors) => "rock beats scissors",
-  %w(paper rock) => "paper beats rock",
-  %w(paper spock) => "paper beats spock",
-  %w(scissors lizard) => "scissors beats lizard",
-  %w(scissors paper) => "scissors beats paper",
-  %w(paper lizard) => "lizard beats paper",
-  %w(lizard spock) => "lizard beats spock",
-  %w(spock scissors) => "spock beats scissors",
-  %w(spock rock) => "spock beats rock"
-}
-
-
-
-
-def validate_choice(user_choice)
-  loop do
-    if %w(r p s l sp).include?(user_choice)
-      break
-    else
-    prompt("Please make a valid choice: (r, p, s, l, sp)")
-    user_choice = gets.chomp.downcase.to_s
-    end
-  end
-end
+COMPUTER_OPTIONS = %w(rock paper scissors lizard spock)
 
 def prompt(message)
-  puts("#{message}")
+  puts message.to_s
 end
 
 def system_clear
-  system('clr') || system('clear')
+  system('cls') || system('clear')
 end
 
 def line_break
-  puts "--------------------------"
+  prompt("====================")
 end
 
-def player_winner?(player1,player2)
-   WINNING_COMBINATION[player1].include?(player2)
+def validate_choice(choice)
+  %w(r p s l sp).include?(choice)
+end
+
+def calc_score(player_score, computer_score)
+  player_score == 5 || computer_score == 5
+end
+
+def winner(player_score, computer_score)
+  if player_score > computer_score
+    prompt("You win!!!...Good job!")
+  else
+    prompt("Computer won....Sorry:(")
+  end
+end
+
+def player_wins(player_choice, computer_choice)
+  WIN_CHOICES[PLAYER_CONVERT_CHOICES[player_choice]].include?(computer_choice)
+end
+
+def computer_wins(player_choice, computer_choice)
+  WIN_CHOICES[computer_choice].include?(PLAYER_CONVERT_CHOICES[player_choice])
 end
 
 msg1 = <<-sm
-
 --------------------------------
   ***************************
         Welcome to
@@ -69,6 +60,18 @@ rock paper scissors lizard spock
           game
   ***************************
 --------------------------------
+
+Here are The Rules of The Game:
+^^ Scissors cuts Paper
+^^ Paper covers Rock
+^^ Rock crushes Lizard
+^^ Lizard poisons Spock
+^^ Spock smashes Scissors
+^^ Scissors decapitates Lizard
+^^ Lizard eats Paper
+^^ Paper disproves Spock
+^^ Spock vaporizes Rock
+^^ Rock crushes Scissors
 sm
 
 msg2 = <<-sm
@@ -80,29 +83,80 @@ l => lizard
 sp => spock
 sm
 
-choice = ''
-user_score = 0
-pc_score = 0
-computer_choice = Computer_choices
-Both_choices = [Convert_choices[choice], computer_choice]
-
 prompt(msg1)
-loop do
-  prompt(msg2)
+player_choice = ''
+player_score = 0
+computer_score = 0
+round = 1
+
+loop do # main loop
+
+  loop do # inner loop
+
+    prompt("Round: #{round}")
+    round += 1
+    line_break
+    prompt(msg2)
+
+    player_choice = gets.downcase.chomp.to_s
+      loop do
+        if validate_choice(player_choice)
+      break
+        else
+      prompt("Invalid choice. Please make a choice (r,p,s,l,sp)")
+      player_choice = gets.chomp.downcase.to_s
+        end
+      end
+
+    computer_choice = COMPUTER_OPTIONS.sample
+    prompt("=> #{computer_choice}")
+    line_break
+
+    puts "You chose #{PLAYER_CONVERT_CHOICES[player_choice]}"
+    puts "computer chose #{computer_choice}"
+    if player_wins(player_choice, computer_choice)
+      puts "You win "
+    player_score += 1
+    elsif computer_wins(player_choice, computer_choice)
+      puts "Computer wins"
+    computer_score += 1
+    else
+      puts "its a tie!"
+    end
+
+    line_break
+
+    prompt("Player score:   #{player_score}")
+    prompt("Computer score: #{computer_score}")
+
+    break if calc_score(player_score, computer_score)
+
+  end
+  system_clear
+  prompt("Final Score")
+  prompt("Player score:   #{player_score}")
+  prompt("Computer score: #{computer_score}")
+  winner(player_score, computer_score)
+
   line_break
-  choice = gets.chomp.downcase.to_s
-  validate_choice(choice)
-  computer_choice
-  line_break
-  prompt("you chose:            #{Convert_choices[choice]}")
-  prompt("computer chose:       #{computer_choice}")
-  prompt("#{Result_display[Both_choices]}")
-   if player_winner?(Convert_choices[choice],computer_choice)
-     prompt("You win")
-   else
-     prompt("Computer wins")
-   end
+
+  prompt("Would you like to play again? ('y' or 'n')")
+  answer = gets.chomp
+
+  loop do
+    break if answer.downcase == 'y' || answer.downcase == 'n'
+    prompt("Please enter a vaild answer (y/n)")
+    answer = gets.chomp
+  end
+
+  break unless answer.downcase == 'y'
+  player_score = 0
+  computer_score = 0
+  round = 1
+  system_clear
+
 end
 
+system_clear
 
-
+p 'Thank you. Goodbye!'
