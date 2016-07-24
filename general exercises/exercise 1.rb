7cecf8141e2f9f3580a2e3097a330d1aba0d71be
Nawar Yossef <nawarpianist@gmail.com>
2016-07-23 19:47:08 -0700

    new exercise solutions

---
+++ general exercises/exercise 1.rb
@@ -0,0 +1,25 @@
+#prbolem: 
+#Write a method that determines and returns the ASCII string value of a string that is passed in as an argument. 
+#The ASCII string value is the sum of the ASCII values of every character in the string. 
+#(You may use String.ord to determine the ASCII value of a character.)
+
+#solution:
+
+# use string.ord to get the ASCII values for each char
+#use array as data structure to calculate the sum of all the ASCII values
+# store the values into a new array
+# two ways to calculate the sum in the array: 1) the 'inject' method, 2) iteration and adding values to a new variable (total)
+
+
+def ascii_value(string)
+   arr = []
+   string.chars {|c| arr << c.ord}
+   arr.inject(&:+).to_i
+end
+
+ascii_value('Four score')
+ascii_value('')
+
+
+
+
