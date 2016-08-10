2465731aab95c1ac1cfe6b65fe9fa5b4f4c0481e
Nawar Yossef <nawarpianist@gmail.com>
2016-08-09 19:31:00 -0700

    ttt file update

--- class 100/tic_tac_toe_game.rb
+++ class 100/tic_tac_toe_game.rb
@@ -6,7 +6,13 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def clear_system
+  system 'clear'
+end
+
 def display_board(brd)
+  clear_system
+  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts "--------------"
   puts "    |    |   "
   puts " #{brd[1]}  | #{brd[2]}  | #{brd[3]}   "
@@ -74,23 +80,29 @@ def detect_winner(brd)
   nil
 end
 
-board = initialize_board
-loop do
+loop do # outer loop
+  board = initialize_board
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
 
-  player_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "it's a tie!"
+  end
 
-  computer_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
+  prompt "Play again?(y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
-
-if someone_won?(board)
-  prompt "#{detect_winner(board)} won!"
-else
-  prompt "it's a tie!"
-end
-
-
+prompt "Thank you for playing Tic Tac Toe. Goodbye!"
