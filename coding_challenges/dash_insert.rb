2db58cc271e8938d8aadbdd407992fe814cc7836
Nawar Yossef <nawarpianist@gmail.com>
2016-09-03 21:31:27 -0700

    delete coding challenge

--- coding_challenges/dash_insert.rb
+++ coding_challenges/dash_insert.rb
@@ -1,14 +1,15 @@
-# Challenge
-# Using the Ruby language, have the function DashInsert(str) 
+# Challenge :
+# using the Ruby language, have the function DashInsert(str) 
 # insert dashes ('-') between each two odd numbers in str. 
-# For example: if str is 454793 the output should be 4547-9-3.
-# Don't count zero as an odd number. 
+# For example: if str is 454793 the output should be 4547-9-3. Don't count zero as an odd number. 
 
-# Solution : 
-def DashInsert(str)
-  str.chars.map.with_index {|n, i| (n.to_i.odd? && str[i - 1].to_i.odd? && i != 0) ? '-' + n : n }.join('')
-end
+# Solution :
+def DashInsert(num)
+  string_for_num = num.to_s
+  result = string_for_num.chars.map.with_index {|n,index| (n.to_i.odd? && string_for_num[index - 1].to_i.odd? && index != 0) ? '-' + n : n}
+  puts result.join
+  end
 
-DashInsert('99946') == '9-9-946'
-DashInsert('56730')  == '567-30'
-DashInsert('54793')  == '4547-9-3'
+DashInsert(454793) == 4547-9-3
+DashInsert(99946) == 9-9-946
+DashInsert(56730) == 567-30
