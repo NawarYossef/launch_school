b92202a94eeaeff7f7ec40f8b100ab75f4a0b700
NawarYossef <nawaryossef2@gmail.com>
2016-10-06 17:47:38 -0500

    new solution

--- coding_challenges/ab_check.rb
+++ coding_challenges/ab_check.rb
@@ -7,8 +7,7 @@
 # the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b).
 # Otherwise return the string false.
 
-# Solution :
-
+# Solution (1) :
 def ABCheck(str)
   letter_a_index = str.rindex('a')
   letter_b_index = str.rindex('b')
@@ -18,6 +17,22 @@ def ABCheck(str)
    else
      puts 'false'
    end
+end
+
+
+# Solution (2) :
+def AlphabetSoup(string)
+  a_indexes, b_indexes,total = [],[],0
+  string.chars.each_with_index {|char,idx| a_indexes << idx if char == 'a'}
+  string.chars.each_with_index {|char,idx| b_indexes << idx if char == 'b'}
+  b_indexes.each do |idx|
+    a_indexes.each do |a_idx|
+      total = idx - a_idx
+      break if total < 4
+      total = 0
+    end
+  end
+  total == 0 ? puts("true") : puts("false")
 end
 
 ABCheck("after badly") == 'false'
